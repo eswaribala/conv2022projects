cd05bf4600bc01ac41c042f63f2f724769cec3bf
eswaribala <parameswaribala@gmail.com>
2022-02-14 08:42:13 +0530

    updated

--- customerapi/src/main/java/com/virtusa/customerapi/CustomerapiApplication.java
+++ customerapi/src/main/java/com/virtusa/customerapi/CustomerapiApplication.java
@@ -3,6 +3,7 @@ package com.virtusa.customerapi;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
 @SpringBootApplication
 public class CustomerapiApplication {
 
